dfdbb73322213ef065e798f591d3366dcd120f2a
soul1on1fire <soul1on1fire@gmail.com>
2024-12-15 13:55:10 -0800

    Change user response

--- OnlineShop.Core/Dto/Auth/RegisterUserResponse.cs
+++ OnlineShop.Core/Dto/Auth/RegisterUserResponse.cs
@@ -2,9 +2,8 @@
 
 public class RegisterUserResponse : Response
 {
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required string Email { get; set; }
-    public required string UserName { get; set; }
-    public required string Password { get; set; }
+    public RegisterUserResponse() : base(true, null)
+    {
+        
+    }
 }
