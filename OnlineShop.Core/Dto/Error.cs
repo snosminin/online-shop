cfe541c36d395e0782cd96957a5004462c08d074
Sergei <osminin-s@fbconsult.ru>
2024-12-19 19:13:55 +0600

    Remove unused

--- OnlineShop.Core/Dto/Error.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShop.Core.Dto
-{
-    public class Error : BaseResponse<object>
-    {
-        public Error(string errorMessage) : base(null,false, errorMessage)
-        {
-        }
-    }
-}
--- OnlineShop.Core/Dto/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineShop.Core.Dto;
-
-public class Response<T>:BaseResponse<T>
-{
-    protected Response(T data) : base(data, true,string.Empty)
-    {
-        
-    }
-}
