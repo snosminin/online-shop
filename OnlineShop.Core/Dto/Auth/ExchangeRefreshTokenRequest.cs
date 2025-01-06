918660e50c0c817580e1d006f33a4ad24ff7728d
Sergei <osminin-s@fbconsult.ru>
2025-01-06 18:45:03 +0600

    Remove unused classes

--- OnlineShop.Core/Dto/Auth/ExchangeRefreshTokenRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OnlineShop.Core.Dto.Auth;
-
-public class ExchangeRefreshTokenRequest
-{
-    public required string AccessToken { get; set; }
-    public required string RefreshToken { get; set; }
-}
